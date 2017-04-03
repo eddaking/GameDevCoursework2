7386aafec9bd9a8684481b4d672ea272a8482bc4
Edmund <ek7g14@ston.ac.uk>
2017-04-03 18:18:02 +0100

    added restart button

--- GameDevCourseWork2/Assets/ClickHandle.cs
+++ GameDevCourseWork2/Assets/ClickHandle.cs
@@ -6,25 +6,34 @@ public class ClickHandle : MonoBehaviour
 {
     public List<int> flagsToFalse = new List<int>();
     public List<int> flagsToTrue = new List<int>();
-    //public List<int> requiredTrueFlags = new List<int>();
-    //public List<int> requiredFalseFlags = new List<int>();
+    public float timeout;
+
+    private bool timing = false;
+    private float timeSinceTrigger = 0;
+
+    void Update()
+    {
+
+        //after a certain ammount of time unmark the clicked flags
+        //to provent bugs arising for click event being flagged before prerequisitis have been met.
+        timeSinceTrigger += Time.deltaTime;
+        if(timeSinceTrigger >= timeout)
+        {
+            timeSinceTrigger = 0;
+            timing = false;
+            foreach (int i in flagsToFalse)
+            {
+                GlobalVars.setFlag(i, true);
+            }
+            foreach (int i in flagsToTrue)
+            {
+                GlobalVars.setFlag(i, false);
+            }
+        }
+    }
 
     public void OnMouseDown()
     {
-        //foreach(int i in requiredFalseFlags)
-        //{
-        //    if (GlobalVars.getFlag(i))
-        //    {
-        //        return;
-        //    }
-        //}
-        //foreach(int i in requiredTrueFlags)
-        //{
-        //    if (!GlobalVars.getFlag(i))
-        //    {
-        //        return;
-        //    }
-        //}
         foreach (int i in flagsToFalse)
         {
             GlobalVars.setFlag(i, false);
@@ -33,5 +42,6 @@ public class ClickHandle : MonoBehaviour
         {
             GlobalVars.setFlag(i, true);
         }
+        timing = true;
     }
 }
---
+++ GameDevCourseWork2/Assets/RestartScript.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class RestartScript : MonoBehaviour {
+
+    public void ReloadLevel()
+    {
+        //Application.LoadLevel(Application.loadedLevel);
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+}
