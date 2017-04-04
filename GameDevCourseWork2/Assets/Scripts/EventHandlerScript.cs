5aae3c2fbab37853a35d8e3e5f01d5352b802646
Edmund <ek7g14@ston.ac.uk>
2017-04-04 21:40:49 +0100

    half implemented level2

--- GameDevCourseWork2/Assets/Scripts/EventHandlerScript.cs
+++ GameDevCourseWork2/Assets/Scripts/EventHandlerScript.cs
@@ -30,26 +30,30 @@ public class EventHandlerScript : MonoBehaviour {
     {
         //events
         //StoryEvent newSE = new StoryEvent(List < int > flagsRequiredTrue, List < int > flagsRequiredFalse, float startTimeTrig, float endTimeTrig, List < GameObject > targets, List < Vector3 > destinations, List < float > speeds, List < int > flagTrue, List < int > flagFalse)
-        StoryEvent frogArmorDrop = new StoryEvent(new List<int>(new int[] { 1 }), new List<int>(new int[] { 17 }), 0, float.MaxValue, new List<GameObject>(new GameObject[] { gameObjectsToReference[9] }), new List<Vector3>(new Vector3[] { new Vector3(0f, 0f, 0f) }), new List<float>(new float[] { 3f }), new List<int>(new int[] { 17 }), new List<int>(new int[] { 1 }));
+        StoryEvent frogArmorDrop = new StoryEvent(new List<int>(new int[] { 1 }), new List<int>(new int[] { 17 }), 0, float.MaxValue, new List<GameObject>(new GameObject[] { gameObjectsToReference[9] }), new List<Vector3>(new Vector3[] { new Vector3(-21.3f, 2.7f, 29.25f) }), new List<float>(new float[] { 10f }), new List<int>(new int[] { 17 }), new List<int>(new int[] { 1 }));
         events.Add(frogArmorDrop);
-        StoryEvent dropStalegEmpty = new StoryEvent(new List<int>(new int[] { 3 }), new List<int>(new int[] { 11, 15 }), 0, float.MaxValue, new List<GameObject>(new GameObject[] { gameObjectsToReference[3] }), new List<Vector3>(new Vector3[] { new Vector3(0f, 0f, 0f) }), new List<float>(new float[] { 3f }), new List<int>(new int[] { 15 }), new List<int>(new int[] { 3 }));
+        StoryEvent dropStalegEmpty = new StoryEvent(new List<int>(new int[] { 3 }), new List<int>(new int[] { 6, 11, 15 }), 0, float.MaxValue, new List<GameObject>(new GameObject[] { gameObjectsToReference[3] }), new List<Vector3>(new Vector3[] { new Vector3(113f, 40f, 92f) }), new List<float>(new float[] { 15f }), new List<int>(new int[] { 15 }), new List<int>(new int[] { 3 }));
         events.Add(dropStalegEmpty);
-        StoryEvent dropStalegSnake = new StoryEvent(new List<int>(new int[] { 3, 11 }), new List<int>(new int[] { 15 }), 0, float.MaxValue, new List<GameObject>(new GameObject[] { gameObjectsToReference[3], gameObjectsToReference[7] }), new List<Vector3>(new Vector3[] { new Vector3(0f, 0f, 0f), new Vector3(0f, 0f, 0f) }), new List<float>(new float[] { 3f, float.MaxValue }), new List<int>(new int[] { 12, 15 }), new List<int>(new int[] { 3 }));
+        StoryEvent dropStalegSnake = new StoryEvent(new List<int>(new int[] { 3, 11 }), new List<int>(new int[] { 15 }), 0, float.MaxValue, new List<GameObject>(new GameObject[] { gameObjectsToReference[3], gameObjectsToReference[6], gameObjectsToReference[7] }), new List<Vector3>(new Vector3[] { new Vector3(113f, 40f, 92f), new Vector3(110f, 36.5f, 85f), new Vector3(102.5f, 37f, 79.5f) }), new List<float>(new float[] { 15f, 15f, float.MaxValue }), new List<int>(new int[] { 12, 15 }), new List<int>(new int[] { 3 }));
         events.Add(dropStalegSnake);
-        StoryEvent frogMove1ToCave = new StoryEvent(new List<int>(new int[] { 5 }), new List<int>(new int[] {  }), 5, 7, new List<GameObject>(new GameObject[] { gameObjectsToReference[5] }), new List<Vector3>(new Vector3[] { new Vector3(109f, 43f, 85f) }), new List<float>(new float[] { 50f }), new List<int>(new int[] { 6 }), new List<int>(new int[] { 5 }));
+        StoryEvent dropStalegFrog = new StoryEvent(new List<int>(new int[] { 3, 6 }), new List<int>(new int[] { 15 }), 0, float.MaxValue, new List<GameObject>(new GameObject[] { gameObjectsToReference[3], gameObjectsToReference[5], gameObjectsToReference[7] }), new List<Vector3>(new Vector3[] { new Vector3(113f, 40f, 92f), new Vector3(109f, 32f, 85f), new Vector3(102.5f, 37f, 79.5f) }), new List<float>(new float[] { 15f, 15f, float.MaxValue }), new List<int>(new int[] { 21, 15 }), new List<int>(new int[] { 3 }));
+        events.Add(dropStalegFrog);
+        StoryEvent frogMove1ToCave = new StoryEvent(new List<int>(new int[] { 5 }), new List<int>(new int[] {  }), 5, 7, new List<GameObject>(new GameObject[] { gameObjectsToReference[5] }), new List<Vector3>(new Vector3[] { new Vector3(109f, 38f, 85f) }), new List<float>(new float[] { 50f }), new List<int>(new int[] { 6 }), new List<int>(new int[] { 5 }));
         events.Add(frogMove1ToCave);
-        StoryEvent frogMove2ToRock1 = new StoryEvent(new List<int>(new int[] { 6 }), new List<int>(new int[] { }), 10, 11, new List<GameObject>(new GameObject[] { gameObjectsToReference[5] }), new List<Vector3>(new Vector3[] { new Vector3(90f, 36f, 92f) }), new List<float>(new float[] { 15f }), new List<int>(new int[] { 7 }), new List<int>(new int[] { 6 }));
+        StoryEvent frogMove2ToRock1 = new StoryEvent(new List<int>(new int[] { 6 }), new List<int>(new int[] { 21 }), 10, 11, new List<GameObject>(new GameObject[] { gameObjectsToReference[5] }), new List<Vector3>(new Vector3[] { new Vector3(90f, 31f, 92f) }), new List<float>(new float[] { 15f }), new List<int>(new int[] { 7 }), new List<int>(new int[] { 6 }));
         events.Add(frogMove2ToRock1);
-        StoryEvent frogMove3ToRock2 = new StoryEvent(new List<int>(new int[] { 7 }), new List<int>(new int[] { }), 11, 12, new List<GameObject>(new GameObject[] { gameObjectsToReference[5] }), new List<Vector3>(new Vector3[] { new Vector3(79f, 24f, 92f) }), new List<float>(new float[] { 15f }), new List<int>(new int[] { 8 }), new List<int>(new int[] { 7 }));
+        StoryEvent frogMove3ToRock2 = new StoryEvent(new List<int>(new int[] { 7 }), new List<int>(new int[] { }), 11, 12, new List<GameObject>(new GameObject[] { gameObjectsToReference[5] }), new List<Vector3>(new Vector3[] { new Vector3(79f, 19f, 92f) }), new List<float>(new float[] { 15f }), new List<int>(new int[] { 8 }), new List<int>(new int[] { 7 }));
         events.Add(frogMove3ToRock2);
-        StoryEvent frogMove4ToRock3 = new StoryEvent(new List<int>(new int[] { 8 }), new List<int>(new int[] { }), 12, 13, new List<GameObject>(new GameObject[] { gameObjectsToReference[5] }), new List<Vector3>(new Vector3[] { new Vector3(69f, 11f, 92f) }), new List<float>(new float[] { 15f }), new List<int>(new int[] { 9 }), new List<int>(new int[] { 8 }));
+        StoryEvent frogMove4ToRock3 = new StoryEvent(new List<int>(new int[] { 8 }), new List<int>(new int[] { }), 12, 13, new List<GameObject>(new GameObject[] { gameObjectsToReference[5] }), new List<Vector3>(new Vector3[] { new Vector3(69f, 6f, 92f) }), new List<float>(new float[] { 15f }), new List<int>(new int[] { 9 }), new List<int>(new int[] { 8 }));
         events.Add(frogMove4ToRock3);
-        StoryEvent frogMove5ToPad = new StoryEvent(new List<int>(new int[] { 9, 19 }), new List<int>(new int[] { 18 }), 13, 14, new List<GameObject>(new GameObject[] { gameObjectsToReference[5] }), new List<Vector3>(new Vector3[] { new Vector3(57f, 5f, 84f) }), new List<float>(new float[] { 15f }), new List<int>(new int[] { 16 }), new List<int>(new int[] { 9 }));
+        StoryEvent frogMove5ToPad = new StoryEvent(new List<int>(new int[] { 9, 19 }), new List<int>(new int[] { 18 }), 13, 14, new List<GameObject>(new GameObject[] { gameObjectsToReference[5] }), new List<Vector3>(new Vector3[] { new Vector3(57f, 0f, 84f) }), new List<float>(new float[] { 15f }), new List<int>(new int[] { 16 }), new List<int>(new int[] { 9 }));
         events.Add(frogMove5ToPad);
-        StoryEvent padMoveWOFrog = new StoryEvent(new List<int>(new int[] { 2, 19 }), new List<int>(new int[] { 16, 18 }), 0, float.MaxValue, new List<GameObject>(new GameObject[] { gameObjectsToReference[2] }), new List<Vector3>(new Vector3[] { new Vector3(-29f, 0f, 84f) }), new List<float>(new float[] { 2f }), new List<int>(new int[] { 18 }), new List<int>(new int[] { 2, 19 }));
+        StoryEvent padMoveWOFrog = new StoryEvent(new List<int>(new int[] { 2, 19 }), new List<int>(new int[] { 16, 18 }), 0, float.MaxValue, new List<GameObject>(new GameObject[] { gameObjectsToReference[2] }), new List<Vector3>(new Vector3[] { new Vector3(-11f, -10f, 10f) }), new List<float>(new float[] { 4f }), new List<int>(new int[] { 18 }), new List<int>(new int[] { 2, 19 }));
         events.Add(padMoveWOFrog);
-        StoryEvent padMoveWFrog = new StoryEvent(new List<int>(new int[] { 2, 16, 19 }), new List<int>(new int[] { 18 }), 0, float.MaxValue, new List<GameObject>(new GameObject[] { gameObjectsToReference[2], gameObjectsToReference[5] }), new List<Vector3>(new Vector3[] { new Vector3(-29f, 0f, 84f), new Vector3(-29f, 5f, 84f) }), new List<float>(new float[] { 2f, 2f }), new List<int>(new int[] { 18 }), new List<int>(new int[] { 2, 19 }));
+        StoryEvent padMoveWFrog = new StoryEvent(new List<int>(new int[] { 2, 16, 19 }), new List<int>(new int[] { 18 }), 0, float.MaxValue, new List<GameObject>(new GameObject[] { gameObjectsToReference[2], gameObjectsToReference[5] }), new List<Vector3>(new Vector3[] { new Vector3(-11f, -10f, 10f), new Vector3(-11f, -10f, 10f) }), new List<float>(new float[] { 2f, 2f }), new List<int>(new int[] { 18 }), new List<int>(new int[] { 2, 19 }));
         events.Add(padMoveWFrog);
+        StoryEvent snakeMove1ToCave = new StoryEvent(new List<int>(new int[] { 10 }), new List<int>(new int[] { }), 14, 15, new List<GameObject>(new GameObject[] { gameObjectsToReference[6] }), new List<Vector3>(new Vector3[] { new Vector3(110f, 39f, 85f) }), new List<float>(new float[] { 50f }), new List<int>(new int[] { 11 }), new List<int>(new int[] { 10 }));
+        events.Add(snakeMove1ToCave);
 
         //flag 0 - click branch
         //flag 1 - click armour type
@@ -62,6 +66,7 @@ public class EventHandlerScript : MonoBehaviour {
         //flag 8 - frog@pos3
         //flag 9 - frog@pos4
         //flag 10 - snake@pos0
+
         //flag 11 - snake@pos1
         //flag 12 - snake dead
         //flag 13 - squirel@pos0
@@ -72,8 +77,11 @@ public class EventHandlerScript : MonoBehaviour {
         //flag 18 - pad moving
         //flag 19 - pad@start
         //flag 20 - pad@end
-        GlobalVars.setFlagArray(new List<bool>(new bool[] { false, false, false, false, false, true, false, false, false, false, false, 
-                                                            true, false, false, true, false, false, false, false, true, false }));
+
+        //flag 21 - frog dead
+        GlobalVars.setFlagArray(new List<bool>(new bool[] { false, false, false, false, false, true, false, false, false, false, true, 
+                                                            false, false, false, true, false, false, false, false, true, false,
+                                                            false}));
     }
 
     private void genLevelTestEvents()
